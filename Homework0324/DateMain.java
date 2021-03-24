0be23359981a42b7044da2cb8a0dc5fb94e03c44
jb8049 <jb8049@naver.com>
2021-03-24 22:09:27 +0900

    dfdsf

--- Homework0324/DateMain.java
+++ Homework0324/DateMain.java
@@ -1,48 +1,74 @@
 package Homework0324;
 
-import java.util.Calendar;
-import java.util.Scanner;
-
 public class DateMain {
 	public static void main(String[] args) {
+
 		
-		String[] dayArr = {"","일","월","화","수","목","금","토"};
-		Calendar c = Calendar.getInstance();
+		DateMenu dm= new DateMenu();
+		dm.dateMenu();
 		
-		//int year = c.get(Calendar.YEAR);  //현재 년도
-		//int month = c.get(Calendar.MONTH)+1; //현재 월 
-		//int date = c.get(Calendar.DATE); // 현재 일
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("선택하세요 (1.특정년도 2.특정월 3.종료 )=> ");
-		int inputNum = sc.nextInt();
-		sc.nextLine();
-		
-		if(inputNum ==1) {
-			System.out.println("년도를 입력하세요. ");
-			int inputYear= sc.nextInt();
-			sc.nextLine();
-			//입력받으면 
-			c.set(inputYear, 1, 1); //2020년을 입력하면 2020년으로 변경함, 년, 월, 일 지정, 이거를 반복문에 넣으면 의미없는 초기화를 계속하는 것
-		
-			//get을 이용해서 2020년 1월 1일부터 출력하기
-			
-			//c.get
-			
-			
-			for(int i=0; i<12; i++) {		
-				int month = c.get(Calendar.MONTH); // 월이 나와야함
-				System.out.println(month);
-			}
-			// 만약 2020을 입력하면
-			//for( int i=0; i<)
-		}
-		
+//		<처음에 main 메소드에 했다가 객체로 다시..>
+//		String[] dayArr = { "", "일", "월", "화", "수", "목", "금", "토" }; // 1(일요일)~7(토요일)까지
+//
+//		Calendar c = Calendar.getInstance(); // 현재 날짜 받기
+//
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("선택하세요 (1.특정년도 2.특정월 3.종료 )=> ");
+//		int inputNum = sc.nextInt();
+//		sc.nextLine();
+//
+//		if (inputNum == 1) {
+//			System.out.println("년도를 입력하세요: ");
+//			int inputYear = sc.nextInt();
+//			sc.nextLine();
+//			// 1-1: 1월, 2-1: 2월 3-1: 3월, 4-1: 4월, 5-1 5월 ~ 12-1: 12월
+//			// c.set(inputYear, i-1, 1); //2020년을 입력하면 2020년으로 변경함, 년, 월, 일 지정, 이거를 반복문에 넣으면
+//			int day; // 무슨 요일인지 확인
+//			int lastday; // 각 월의 최댓값
+//
+//			for (int i = 1; i < 13; i++) {
+//				c.set(inputYear, i - 1, 1);
+//				System.out.println(); // 출력문 잘보이게함
+//				System.out.println("<< " + inputYear + "년 " + i + "월 >>");
+//				lastday = c.getActualMaximum(Calendar.DAY_OF_MONTH); // lastday까지 수를 반복적으로 출력하면 해당 월의 달력이 출력됨
+//				day = c.get(Calendar.DAY_OF_WEEK);
+//				for (int j = 1; j <= lastday; j++) {
+//					System.out.print(" " + j + "일=" + dayArr[day]);
+//					day++;
+//					if (day == 8) {
+//						day = 1; // dayArr 배열 크기가 7(토요일)이 되었을 때, 다시 1(일요일)로 이동
+//					}
+//
+//				}
+//				System.out.println();
+//
+//			}
+//
+//		} else if (inputNum == 2) {
+//			System.out.println("년도를 입력하세요: ");
+//			int inputYear = sc.nextInt();
+//			sc.nextLine();
+//			System.out.println("월을 입력하세요: ");
+//			int inputMonth = sc.nextInt();
+//			sc.nextLine();
+//			int day; // 무슨 요일인지 확인
+//			int lastday; // 각 월의 최댓값
+//
+//			c.set(inputYear, inputMonth - 1, 1);
+//			lastday = c.getActualMaximum(Calendar.DAY_OF_MONTH); // lastday까지 수를 반복적으로 출력하면 해당 월의 달력이 출력됨
+//			day = c.get(Calendar.DAY_OF_WEEK);
+//			for (int j = 1; j <= lastday; j++) {
+//				System.out.print(" " + j + "일=" + dayArr[day]);
+//				day++;
+//				if (day == 8) {
+//					day = 1; // dayArr 배열 크기가 7(토요일)이 되었을 때, 다시 1(일요일)로 이동
+//				}
+//			}
+//
+//		}else if(inputNum==3) {
+//			System.out.println("종료");
+//		}
+
 	}
-	
-
 }
-
-
-
-// 2020년 1월부터 
---
+++ Homework0324/DateMenu.java
@@ -0,0 +1,81 @@
+package Homework0324;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class DateMenu {
+
+	Scanner sc = new Scanner(System.in);
+
+	private String[] dayArr = { "", "일", "월", "화", "수", "목", "금", "토" };
+	private int inputNum;
+	private int inputYear;
+	private int inputMonth;
+	private int day; // 무슨 요일인지 확인
+	private int lastday; // 각 월의 최댓값
+
+	Calendar c = Calendar.getInstance();
+
+	public int choiceMenu() {
+		System.out.println("선택하세요 (1.특정년도 2.특정월 3.종료 )=> ");
+		int inputNum = sc.nextInt();
+		sc.nextLine();
+
+		return inputNum;
+	}
+
+	public void dateMenu() {
+		inputNum = choiceMenu();
+		if (inputNum == 1) {
+			System.out.println("년도를 입력하세요: ");
+			inputYear = sc.nextInt();
+			sc.nextLine();
+//			1-1: 1월, 2-1: 2월 3-1: 3월, 4-1: 4월, 5-1 5월 ~ 12-1: 12월
+//			c.set(inputYear, i-1, 1); //2020년을 입력하면 2020년으로 변경함, 년, 월, 일 지정
+//			int day; // 무슨 요일인지 확인
+//			int lastday; // 각 월의 최댓값
+
+			for (int i = 1; i < 13; i++) {
+				c.set(inputYear, i - 1, 1);
+				System.out.println(); // 출력문 잘보이게함
+				System.out.println("<< " + inputYear + "년 " + i + "월 >>");
+				lastday = c.getActualMaximum(Calendar.DAY_OF_MONTH); // lastday까지 수를 반복적으로 출력하면 해당 월의 달력이 출력됨
+				day = c.get(Calendar.DAY_OF_WEEK);
+				for (int j = 1; j <= lastday; j++) {
+					System.out.print(" " + j + "일=" + dayArr[day]);
+					day++;
+					if (day == 8) {
+						day = 1; // dayArr 배열 크기가 7(토요일)이 되었을 때, 다시 1(일요일)로 이동
+					}
+
+				}
+				System.out.println();
+
+			}
+
+		} else if (inputNum == 2) {
+			System.out.println("년도를 입력하세요: ");
+			inputYear = sc.nextInt();
+			sc.nextLine();
+			System.out.println("월을 입력하세요: ");
+			inputMonth = sc.nextInt();
+			sc.nextLine();
+
+			c.set(inputYear, inputMonth - 1, 1);
+			lastday = c.getActualMaximum(Calendar.DAY_OF_MONTH); // lastday까지 수를 반복적으로 출력하면 해당 월의 달력이 출력됨
+			day = c.get(Calendar.DAY_OF_WEEK);
+			for (int j = 1; j <= lastday; j++) {
+				System.out.print(" " + j + "일=" + dayArr[day]);
+				day++;
+				if (day == 8) {
+					day = 1; // dayArr 배열 크기가 7(토요일)이 되었을 때, 다시 1(일요일)로 이동
+				}
+			}
+
+		} else if (inputNum == 3) {
+			System.out.println("종료");
+		}
+
+	}
+
+}
